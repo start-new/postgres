bf99e1e609a00d10233e2e1e6a29e2f95c1b997d
Thierry Chappuis <tchappui@users.noreply.github.com>
2024-05-12 18:13:43 +0200

    Update post_gen_project.py

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Automatically generates a basic docker-compose file to install and use the postg
 
 ## Requirements & Install
 
-To use the generated file, you need to have Docker installed on your computer. Then:
+To use this project, you will need to install:
+- [Docker Desktop](https://docs.docker.com/desktop/)
+- [uv](https://docs.astral.sh/uv/getting-started/installation/)
 
-- Install Cookiecutter from PyPI using `pip install cookiecutter`
-- Generate the docker-compose file using `cookiecutter gh:start-new/postgres`
+- Generate the compose.yml file using `uvx cookiecutter gh:start-new/postgres`
 - Answer the questions
-- Move to the newly-created project folder using `cd <project_slug>`
-- Launch the database using `docker-compose up` or `docker-compose up -d`
+- Launch the database using `docker compose up -d`
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,16 +3,17 @@ import random
 import shutil
 import secrets
 
+
 def move_files():
     Path("compose.yml").rename(Path("../compose.yml"))
 
+
 def delete_project_dir():
     """Removes the project folder."""
-    shutil.rmtree("../{{ cookiecutter.project_slug }}")
+    shutil.rmtree("../{{ cookiecutter._project_dir }}")
 
-def set_flag(
-    file_path, flag, length=64, value=None, formatted=None, *args, **kwargs
-):
+
+def set_flag(file_path, flag, length=64, value=None, formatted=None, *args, **kwargs):
     if value is None:
         value = secrets.token_urlsafe(length)
 
@@ -28,15 +29,26 @@ def set_flag(
 def set_postgres_auto_port():
     postgres_auto_port = random.randint(10000, 65535)
     set_flag(
-        Path("docker-compose.yml"),
+        Path("compose.yml"),
         "!!!SET POSTGRESQL_PORT!!!",
         value=str(postgres_auto_port),
     )
     return postgres_auto_port
 
 
+def set_pgadmin_auto_port():
+    pgadmin_auto_port = random.randint(10000, 65535)
+    set_flag(
+        Path("compose.yml"),
+        "!!!SET PGADMIN_PORT!!!",
+        value=str(pgadmin_auto_port),
+    )
+    return pgadmin_auto_port
+
+
 def set_flags():
     set_postgres_auto_port()
+    set_pgadmin_auto_port()
 
 
 def main():
@@ -45,7 +57,6 @@ def main():
     set_flags()
     move_files()
     delete_project_dir()
-  
 
 
 if __name__ == "__main__":
